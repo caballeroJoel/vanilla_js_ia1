2a4aeaa84cacd5d8b741a16ff944f2a4b6011982
Joel Caballero <caballeroluciajoel@fpllefia.com>
2026-09-14 20:08:49 +0200

    Ex_1

---
+++ app/app.js
@@ -0,0 +1,10 @@
+const divM = document.querySelector("#divMostrar");
+
+let player = "Joel Caballero";
+const MAX_TIRADAS = 3;
+
+divM.innerHTML += "Bienvenido, "+player+" tienes "+ MAX_TIRADAS +" tiradas<br>";
+
+player = "Pepe";
+
+divM.innerHTML += "Bienvenido, "+player+" tienes "+ MAX_TIRADAS +" tiradas<br>";
